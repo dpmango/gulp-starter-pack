cfe839d985f8b729b9c7a44b2e7638d7e5f8b46d
Sergey Khmelevskoy <xs290@me.com>
2019-04-07 23:44:14 +0300

    add babel for javascript:app

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,13 +1,15 @@
-var gulp        = require('gulp');
-var path        = require('path');
-var util        = require('gulp-util');
-var plumber     = require('gulp-plumber');
-var concat      = require('gulp-concat');
-var uglifyJs    = require('gulp-uglify');
-var config      = require('../config');
+var gulp = require('gulp');
+var path = require('path');
+var util = require('gulp-util');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var uglifyJs = require('gulp-uglify');
+var babel = require('gulp-babel');
+var config = require('../config');
 
 gulp.task('javascript:vendor', function() {
-  return gulp.src([
+  return gulp
+    .src([
       path.resolve('bower_components', 'jquery/dist/jquery.min.js'),
       path.resolve('bower_components', 'svg4everybody/dist/svg4everybody.min.js'),
       path.resolve('bower_components', 'picturefill/dist/picturefill.min.js'),
@@ -24,8 +26,8 @@ gulp.task('javascript:vendor', function() {
       path.resolve('bower_components', 'jquery-lazy/plugins/jquery.lazy.picture.min.js'),
       path.resolve('node_modules', 'gsap/src/minified/TweenMax.min.js'),
       path.resolve('node_modules', 'gsap/src/minified/plugins/ScrollToPlugin.min.js'),
-      config.src.js + '/vendor/**/*.js'
-     ])
+      config.src.js + '/vendor/**/*.js',
+    ])
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(concat('vendor.js'))
     .pipe(config.production ? uglifyJs() : util.noop())
@@ -33,27 +35,33 @@ gulp.task('javascript:vendor', function() {
 });
 
 gulp.task('javascript:app', function() {
-  return gulp.src([
+  return gulp
+    .src([
       config.src.js + '/*.js',
       config.src.js + '/modules/**/*.js',
       config.src.components + '/**/*.js',
-     ])
+    ])
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(concat('app.js'))
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      }),
+    )
     .pipe(config.production ? uglifyJs() : util.noop())
     .pipe(gulp.dest(config.dest.js));
 });
 
-gulp.task('javascript', [
-  'javascript:vendor',
-  'javascript:app'
-]);
+gulp.task('javascript', ['javascript:vendor', 'javascript:app']);
 
 gulp.task('javascript:watch', function() {
   gulp.watch(config.src.js + '/vendor/**/*.js', ['javascript:vendor']);
-  gulp.watch([
-    config.src.components + '/**/*.js',
-    config.src.js + '/modules/**/*.js',
-    config.src.js + '/*.js'
-  ], ['javascript:app']);
+  gulp.watch(
+    [
+      config.src.components + '/**/*.js',
+      config.src.js + '/modules/**/*.js',
+      config.src.js + '/*.js',
+    ],
+    ['javascript:app'],
+  );
 });
